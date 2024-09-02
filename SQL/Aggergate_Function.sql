dcd60fcb7c344b5511dc9b20bbc89843b98c09b2
HADAHENO <144498842+HADAHENO@users.noreply.github.com>
2024-09-02 23:38:43 +0300

    Add files via upload

---
+++ SQL/Aggergate_Function.sql
@@ -0,0 +1,24 @@
+
+select * from Employee
+
+Select Max(Salary) as Max_salary
+From Employee
+
+Select Min(Salary) as Min_salary
+From Employee
+
+
+Select Avg(Salary) as Avg_salary
+From Employee
+
+Select Sum(Salary) as Sum_salary
+From Employee
+
+Select Count(Salary) as Count_Employee
+From Employee
+
+Select Count(dno) as Count_Employee
+From Employee
+
+Select Count(*) as Count_Employee
+From Employee
