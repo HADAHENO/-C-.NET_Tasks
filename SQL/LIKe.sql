d7e85700a21dd88da015bd6a6e822ac12f9f5a37
HADAHENO <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 01:27:15 +0300

    Add files via upload

---
+++ SQL/LIKe.sql
@@ -0,0 +1,21 @@
+----like-----------
+select * from Employee 
+where name= 'hu'
+
+select * from Employee 
+where name like 'hu%'
+
+select * from Employee 
+where name like '_A_%'
+
+select * from Employee 
+where name like 'd_%'
+
+select * from Employee 
+where name like '_l_%'
+
+select * from Employee 
+where name like '__[da]%'
+
+select * from Employee 
+where name like '_[a-h]%'
