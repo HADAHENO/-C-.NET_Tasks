1d992d424383f5ec85c77bb9e4c2edaa501985d4
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 20:33:50 +0300

    Add files via upload

---
+++ C# _Basics/insert to show.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter numes of array");
+        string n_string=Console.ReadLine();
+        int.TryParse(n_string, out int n);
+
+        int[ ] nums = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            int.TryParse (s, out nums[i]);
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine("----------");
+            Console.WriteLine(nums[i]);
+        }
+
+    }
+    } 
