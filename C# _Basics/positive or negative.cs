fe1dcf4234d0ea249c3b680f2ecc4e92d2a57d47
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 18:23:43 +0300

    Add files via upload

---
+++ C# _Basics/positive or negative.cs
@@ -0,0 +1,23 @@
+﻿using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter num1-> ");
+
+        string num_string1 = Console.ReadLine();
+
+        int.TryParse(num_string1, out int num1);
+
+        if (num1 >= 0)
+            Console.WriteLine($"{num1} is positive");
+        else
+            Console.WriteLine($"{num1} is negative");
+        }
+
+
+    } 
