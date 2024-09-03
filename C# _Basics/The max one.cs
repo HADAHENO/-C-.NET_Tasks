3a2e5d71c00ef71159449902db113b0e19b18435
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 18:03:32 +0300

    Add files via upload

---
+++ C# _Basics/The max one.cs
@@ -0,0 +1,37 @@
+﻿using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter num1-> ");
+
+        string num_string1 = Console.ReadLine();
+
+        Console.WriteLine("enter num1-> ");
+
+        string num_string2 = Console.ReadLine();
+
+        int.TryParse(num_string1, out int num1);
+        int.TryParse(num_string2, out int num2);
+
+
+        if (num1 > num2)
+        {
+            Console.WriteLine($"{num1} is max");
+        }
+        else if (num1 < num2)
+        {
+            Console.WriteLine($"{num2}is max");
+        }
+        else if (num1 == num2)
+        {
+            Console.WriteLine($"{num1} equale {num2}");
+        }
+        }
+
+
+    } 
