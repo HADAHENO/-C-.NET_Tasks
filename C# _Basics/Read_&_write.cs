0d0d823844d10d38d15fe276cd6cad482982f7f8
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 17:23:10 +0300

    Add files via upload

---
+++ C# _Basics/Read_&_write.cs
@@ -0,0 +1,27 @@
+﻿using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter_your_name");
+        string Name=Console.ReadLine();
+        Console.WriteLine($"your_name_is:{(Name)}");
+        Console.WriteLine("enter_your_age");
+        string age_string = Console.ReadLine();
+
+        int.TryParse(age_string, out int Age);
+
+      
+        Console.WriteLine($"your age is :{(Age)}");
+
+
+
+
+    }
+
+
+}
