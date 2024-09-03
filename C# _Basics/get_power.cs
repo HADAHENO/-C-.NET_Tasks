baa519b1bc1b05d79912e654fb283daa802832ce
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 20:51:35 +0300

    Add files via upload

---
+++ C# _Basics/get_power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string num1_string=Console.ReadLine();
+        string num2_string=Console.ReadLine();
+
+        int.TryParse(num1_string, out int num1);
+        int.TryParse (num2_string, out int num2);
+
+        Console.WriteLine($"{Math.Pow(num1,num2)}");
+
+    }
+    } 
