e3ee10b7a65a48c8c03013234bf7cc750182f971
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 17:46:50 +0300

    Add files via upload

---
+++ C# _Basics/Check_num.cs
@@ -0,0 +1,22 @@
+﻿using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter num-> ");
+
+        string num_string = Console.ReadLine();
+
+        int.TryParse(num_string, out int num1);
+        if (num1 % 3 == 0 && num1 % 4 == 0) 
+            Console.WriteLine("Yes");
+        else
+                Console.WriteLine("No");
+        }
+
+
+    } 
