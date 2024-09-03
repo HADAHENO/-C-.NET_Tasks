98591ee6327bd0a958d5ee3b0e7549f3f5dd634e
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 18:56:12 +0300

    Add files via upload

---
+++ C# _Basics/even or odd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter num1-> ");
+
+        string num_string1 = Console.ReadLine();
+
+        int.TryParse(num_string1, out int num1);
+        if (num1 % 2 == 0) 
+            Console.WriteLine($"{num1}is even");
+        else
+            Console.WriteLine($"{num1}is odd");
+
+        }
+    } 
