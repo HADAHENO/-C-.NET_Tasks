814ad695073e83bf21b9d41c1cd2f24476488d1e
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-03 20:20:37 +0300

    Add files via upload

---
+++ C# _Basics/vowel or consontant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("enter char-> ");
+        string chare=Console.ReadLine();
+        if (chare == "a" || chare == "e" || chare == "I" || chare == "o" || chare == "u")          ///(a,e,I,o,u)
+        {
+            Console.WriteLine("vowel");
+        }
+        else
+            Console.WriteLine("Consonant");
+
+
+    }
+    } 
