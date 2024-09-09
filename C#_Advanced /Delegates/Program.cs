e39ef04c11c3e141d2457911c28487afb9076183
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-09 23:21:04 +0300

    Add files via upload

---
+++ C#_Advanced /Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Delegateee.delegate_squar;
+
+namespace Delegateee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            delegate_squar sqr=new delegate_squar();
+            rectDelegate square = sqr.Area;
+            square += sqr.line;
+            square.Invoke(9, 8);
+
+            Console.WriteLine();
+
+            square.Invoke(10, 3.5);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#_Advanced /Delegates/delegate_squar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegateee
+{
+    internal class delegate_squar
+    {
+      public delegate void rectDelegate(double height,double width);
+        public  void Area( double height,double width)
+        {
+            Console.WriteLine($"the area_deledate: {(width * height)}");
+        }
+        public void line(double height, double width)
+        {
+            Console.WriteLine($"the line_delegate: {(2 * (width + height))}");
+        }
+
+    }
+}
