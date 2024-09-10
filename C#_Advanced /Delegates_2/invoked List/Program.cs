3aa4ef3d9dd20af3eb9004e649f3a4b257883346
Huda_MAHER <144498842+HADAHENO@users.noreply.github.com>
2024-09-10 18:33:03 +0300

    Add files via upload

---
+++ C#_Advanced /Delegates_2/invoked List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Delegateee.delegateCLS;
+using static Delegateee.Program;
+//using static Delegateee.delegate_squar;
+
+namespace Delegateee
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            delgateLIST del = methode1;
+            del += methode2;
+            del += methode3;
+
+
+            int a=int.Parse(Console.ReadLine());
+
+
+            int result = del(a);
+            Console.WriteLine(result);
+
+        }
+    }
+}
